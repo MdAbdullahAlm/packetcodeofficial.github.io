564eadecfe8daad385001b18e4a0ac18a8ec8715
MdAbdullahAlm <alm108187@gmail.com>
2020-08-11 13:39:09 +0600

    add

---
+++ Home.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WORLD OF KNOWLADGE</title>
+    <link rel="icon" type="image/png" href="img/LOGO.png">
+    <link type="text/css" rel="stylesheet" href="css/HomeA.css">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/slider.css">
+    <link rel="stylesheet" href="css/searchBar.css">
+    
+  
+      <style>
+    
+    </style>
+</head>
+
+<body >
+      
+	
+   <div id="MainDiv" >
+
+	<?php        
+		 include('header.php');
+	?>
+	
+	<?php        
+		 include('footer.php');
+	?>	
+        
+        
+
+        </div>
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ footer.php
@@ -0,0 +1,100 @@
+<html>
+<head>
+	<link type="text/css" rel="stylesheet" href="css/footer.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body >
+  <div id="background-image" style="background-image:url('img/footerBG.jpg');  
+  background-repeat:repeat-x; width: 100%; height: 500px; ">
+	
+	
+	   <div id="MainFooter">
+            <div id="footerHeader" >
+                <table>
+                    <tr>
+                        <td>
+                            <div id="footerHeaderLogo"><img src="img/logo.png"/></div>
+                        </td>
+                        <td>
+                            <ul id="footerHeaderSLink">
+                                <li>
+                                    <a href=""><img src="img/j.png"/> </a>
+                                </li>
+                                <li>
+                                    <a href=""><img src="img/google.jpg"/> </a>
+                                </li>
+                                <li>
+                                    <a href=""> <img src="img/Instrogram.png"/></a>
+                                </li>
+                                <li>
+                                    <a href=""> <img src="img/twter.png"/> </a>
+                                </li>
+                                <li>
+                                    <a href=""><img src="img/youtube.png"/></a>
+                                </li>
+                                <li>
+                                    <a href=""><img src="img/whats app.png"/></a>
+                                </li>
+                            </ul>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+            <div id="footerBoby">
+                <table>
+                    <tr>
+
+                        <td>
+                            <ul>
+                                <li><a href="Home.php">Home</a>
+                                </li>
+                                <li><a href="Login.php">Technology</a>
+                                </li>
+                                <li> <a href="Login.php">Computer</a>
+                                </li>
+                            </ul>
+                        </td>
+                        <td>
+                            <ul>
+                                <li> <a href="Login.php">Geography</a>
+                                </li>
+                                <li> <a href="Login.php">Evolution</a>
+                                </li>
+                                <li> <a href="Login.php">Bangladesh</a>
+
+                                </li>
+                            </ul>
+                        </td>
+                        <td>
+                            <ul>
+                                <li> <a href="Login.php">Tourist Place</a>
+                                </li>
+                                <li> <a href="Login.php">Sports</a>
+                                </li>
+                                <li> <a href="Login.php">Cricket</a>
+                                </li>
+                            </ul>
+                        </td>
+                        <td>
+                            <ul>
+                                <li> <a href="Login.php">UNESCO</a>
+                                </li>
+                                <li> <a href="Login.php">Education</a>
+                                </li>
+                                <li> <a href="Login.php">Contact US</a>
+
+                                </li>
+                            </ul>
+                        </td>
+                    </tr>
+                </table>
+                <div id="copyright">copyright : abdullahmamun108187@gmail.com</div>
+            </div>
+
+        </div>
+	
+	
+	</div>
+</body>
+</html>
